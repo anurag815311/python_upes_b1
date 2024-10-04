8953860180ac4da28b39c1a2fcce41e8a5b47cd8
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-04 08:46:36 +0530

    time modules use cases

---
+++ class_work2.py
@@ -0,0 +1,18 @@
+#date :- 04-10-24
+import time 
+ns={12312312,23423423,24234234,2345346456}
+
+def testfn(n):
+    for i in range(0,n):
+        a=i*10
+
+def wrapper(func,n):
+    start_time = time.time() * 1000000
+
+    func(n)
+
+    end_time=time.time()*1000000
+    print(f" for n = {n} \n Execution time is {end_time-start_time}")
+
+for n in ns:
+    wrapper(testfn,n)
