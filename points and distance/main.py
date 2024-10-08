648e78e6b0031dffc5352e1c194d4c86156fc1cc
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-08 09:11:04 +0530

    distance and points

--- points and distance/main.py
+++ points and distance/main.py
@@ -1,12 +1,25 @@
-p1=(0,1)
-p2=(2,4)
-p3=(3,6)
-p4=(7,7)
+import math
+p1={"x":0,"y":1}
+p2={"x":2,"y":4}
+p3={"x":3,"y":6}
+p4={"x":7,"y":7}
+p5={"x":1,"y":0}
 def distance(p1,p2):
-    x1=p1[0]
-    x2=p2[0]
-    y1=p1[1]
-    y2=p2[1]
-    d=((x2-x1)^2 + (y2-y2)^2)**1/2
+    x1=p1["x"]
+    x2=p2["x"]
+    y1=p1["y"]
+    y2=p2["y"]
+    d=((x2-x1)^2 + (y2-y2)^2)**(1/2)
     return d;
 
+def polar(p1):
+    x=p1["x"]
+    y=p1["y"]
+    r=math.sqrt(x**2 + y**2)
+    theta=math.atan2(y,x)
+    phi=math.degrees(theta);
+    return(r,phi)
+#distance between two points 
+print(distance(p3,p4))
+#calculate polar coordinate 
+print(polar(p1))
