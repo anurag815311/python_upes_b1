3fd0b6be4287d4c04512c156d0010369a4000385
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-17 16:15:53 +0530

    understanding se of arguments

---
+++ function/functiondocumentation.py
@@ -0,0 +1,15 @@
+def multiply(a, b):
+
+    """
+    Multiplies two numbers and returns the result.
+    
+    Parameters:
+    a (int, float): The first number.
+    b (int, float): The second number.
+    
+    Returns:
+    int, float: The product of a and b.
+    """
+    return a + b
+
+print(multiply(23,34))
---
+++ function/functionmore.py
@@ -0,0 +1,21 @@
+def introduce(name,age=25):
+    print(f"Hello, my name is {name} and I am {age} years old")
+
+
+def sum_number(number):
+    return sum(number)
+
+
+
+
+"""
+def print_numbers(*args):
+    for i in args:
+        print(i)
+
+print_numbers(1,23,4,6)
+result=sum_number([1,2,3,4,5])
+print(result)
+introduce("John",30)
+introduce("bob")
+"""
