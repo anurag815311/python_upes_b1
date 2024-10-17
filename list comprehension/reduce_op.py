d93d71db5bd5fd5f8f88ac7d2b9d8f00a8099ac1
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-17 19:15:59 +0530

    reduce,map,lambda,filter

--- list comprehension/reduce_op.py
+++ list comprehension/reduce_op.py
@@ -16,3 +16,15 @@ print(product)
 
 result=reduce(multiply,numbers)
 print(result)
+
+#more on map and lambda
+
+def add_ten(number):
+    return number+10
+numbers=[1,2,3,4]
+result=map(add_ten,numbers)
+print(list(result))
+
+#using lambda function
+results=list(map(lambda x:x+10,numbers))
+print(results)
