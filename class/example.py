62a7aee23baf2e24ac98090dc4265182134fdbc3
Anurag Singh <anuragsingh815311@gmail.com>
2024-10-22 08:54:35 +0530

    classes in python example1

--- class/example.py
+++ class/example.py
@@ -3,10 +3,13 @@ class Dog:
     def __init__(self, name, age):
         self.name=name
         self.age=age
+        #self.__age=age-->private property.
     def bark(self):
         return f"{self.name} says woof!"
     def get_age(self):
         return f"{self.name} is {self.age} years old"
+    def __str__(self):
+        return f"{self.name} is a {self.age} year old {self.species}"   #custom representation for object when it is printed.
     
 dog1=Dog("Buddy",3)
 dog2=Dog("Charlie",5)
@@ -14,4 +17,5 @@ dog2=Dog("Charlie",5)
 print(dog2.bark())
 print(dog1.get_age())
 print(dog1.species)
-print(dog1.age)
+#print(dog1.__age)  --> Can't be accessed outside the class..
+print(dog1)#output->Buddy is a 3 year old coins familiars
