4ae64a9fa790682baf6002eb05f7f3f4f08d4a3c
Anurag Singh <anuragsingh815311@gmail.com>
2024-12-10 16:08:34 +0530

    Decorate-practice

--- wrapper/decorator2.py
+++ wrapper/decorator2.py
@@ -1,10 +1,19 @@
 def wrapped(func):
     def innerwrapped(*args,**kwargs):
-        for i in range(1,5):
-            for j in range(1,i):
-                print("*");
+        for i in range(1,50):
+            #for j in range(1,i):
+                print("*",end="");
+        print(" ")
         func(*args,**kwargs)
-        print(" ",end="");
+        for i in range(1,50):
+            #for j in range(1,i):
+                print("*",end="");
+        print(" ")
     return innerwrapped
 
+@wrapped 
+def fun(n):
+    for i in range(1,n):
+        print(i*n);
 
+result=fun(10)
