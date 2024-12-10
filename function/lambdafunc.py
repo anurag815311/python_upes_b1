e94cfd262e5abe377df617c5d40f3498d424e77a
Anurag Singh <anuragsingh815311@gmail.com>
2024-12-10 19:19:23 +0530

    maps and filter

--- function/lambdafunc.py
+++ function/lambdafunc.py
@@ -3,7 +3,7 @@ add=lambda a,b:a+b
 #print(add(3,2))
 numbers=[1,23,2,3]
 squared=map(lambda x:x*x,numbers)
-#print(list(squared))
+print(list(squared))
 
 numbers2=[2,3,4,8]
 even_list=filter(lambda x:x%2==0,numbers2)
